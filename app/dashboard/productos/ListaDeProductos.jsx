eac896ab8be679ea2c0bdcd6054e65ea3343af7a
marlonrodri7 <marlon7piri@gmail.com>
2024-01-13 17:38:51 -0500

    chnage 15

--- app/dashboard/productos/ListaDeProductos.jsx
+++ app/dashboard/productos/ListaDeProductos.jsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import Link from "next/link";
 import React, { Suspense, useEffect, useState } from "react";
@@ -6,29 +6,29 @@ import Image from "next/image";
 import Botones from "./Botones";
 import { convertidordefecha } from "@/app/libs/convertidordefecha";
 
-const ListaDeProductos = ({ productos } ) => {
-  const [total, setTotal] = useState(0)
-  
+const ListaDeProductos = ({ productos }) => {
+  const [total, setTotal] = useState(0);
 
-  
-  const getTotal =()=>{
-    const numero = productos.reduce((acc,current)=>{
-       return acc + current.precio_por_unidad * current.stock
-     
-    },0)
+  const getTotal = () => {
+    const numero = productos.reduce((acc, current) => {
+      return acc + current.precio_por_unidad * current.stock;
+    }, 0);
 
-    setTotal(numero)
-  }
+    setTotal(numero);
+  };
 
-  useEffect(()=>{
-    getTotal()
-  },[productos])
+  useEffect(() => {
+    getTotal();
+  }, [productos]);
 
-  
   return (
     <Suspense>
       <div className="w-full h-full relative overflow-x-auto shadow-md sm:rounded-lg">
-      <span className="w-full  m-auto text-center text-2xl">Dinero Total en Stock: ${total.toFixed(3)}</span>
+        <div className="flex gap-2">
+          {" "}
+          <span className="">Dinero Total en Stock: ${total.toFixed(3)}</span>
+          <span className="">Cantidad De Productos: {productos.length}</span>
+        </div>
 
         <table className="w-full h-full relative text-sm text-left text-gray-500 dark:text-gray-400">
           <thead className="text-xs text-slate-900 uppercase bg-sky-500 dark:bg-gray-900 dark:text-gray-400   ">
@@ -44,21 +44,21 @@ const ListaDeProductos = ({ productos } ) => {
               </th>
 
               <th scope="col" className="px-6 py-3">
-              Presentacion Por Unidad
+                Presentacion Por Unidad
               </th>
-              
+
               <th scope="col" className="px-6 py-3">
-               Costo
+                Costo
               </th>
-              
+
               <th scope="col" className="px-6 py-3">
                 Proveedor
               </th>
               <th scope="col" className="px-6 py-3">
-               Mas Vendido
+                Mas Vendido
               </th>
               <th scope="col" className="px-6 py-3">
-               Categoria
+                Categoria
               </th>
               <th scope="col" className="px-6 py-3">
                 Fecha de Creacion
@@ -81,34 +81,38 @@ const ListaDeProductos = ({ productos } ) => {
                     className=" bg-white border-b dark:bg-gray-800 dark:border-gray-700"
                     key={product._id}
                   >
-                    
-                    <td className="px-6 py-4 ">
-                      {product.nombre}
-                    </td>
+                    <td className="px-6 py-4 ">{product.nombre}</td>
                     <td className="px-6 py-4">${product.precio_por_unidad}</td>
 
-                    <td className="px-6 py-4">{product.presentacion_por_unidad}/{product.unidad}</td>
-                 
+                    <td className="px-6 py-4">
+                      {product.presentacion_por_unidad}/{product.unidad}
+                    </td>
+
                     <td className="px-6 py-4">${product.costo}</td>
                     <td className="px-6 py-4">{product.proveedor}</td>
-                    <td className={ product.mas_vendido ? "px-6 py-4 text-green-700 font-bold" :"px-6 py-4 text-red-700 font-bold"  }>{product.mas_vendido ? "Si" : "No"}</td>
+                    <td
+                      className={
+                        product.mas_vendido
+                          ? "px-6 py-4 text-green-700 font-bold"
+                          : "px-6 py-4 text-red-700 font-bold"
+                      }
+                    >
+                      {product.mas_vendido ? "Si" : "No"}
+                    </td>
                     <td className="px-6 py-4">{product.categoria}</td>
                     <td className="px-6 py-4">
-                      {convertidordefecha(product.createdAt)} 
+                      {convertidordefecha(product.createdAt)}
                     </td>
 
                     <td className="w-max px-2 py-2  flex gap-1 justify-center items-center">
-                     <Botones allproducto={product._id}/>
+                      <Botones allproducto={product._id} />
                     </td>
                   </tr>
                 );
               })
-            )
-            }
-           
+            )}
           </tbody>
         </table>
-
       </div>
     </Suspense>
   );
