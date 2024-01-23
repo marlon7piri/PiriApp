411fb0f4dcca5cf0b50331c57fe0e6876988a0c6
marlonrodri7 <marlon7piri@gmail.com>
2024-01-23 16:12:08 -0500

    add email resend

--- app/home/pedidos/TablaPedidos.jsx
+++ app/home/pedidos/TablaPedidos.jsx
@@ -6,10 +6,13 @@ import React, { useEffect, useState } from "react";
 import { useClientContext } from "../../context/ClientProvider";
 import DeleteIcon from "@/app/icons/DeleteIcon";
 import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import { UrlWeb } from "@/app/libs/UrlWeb";
 
 const TablaPedidos = () => {
   const { pedidos, setPedidos } = useClientContext();
-
+  const [orden, setOrden] = useState([]);
+  const router = useRouter();
 
   const deletePedidos = (id) => {
     const res = pedidos.filter((item) => {
@@ -19,26 +22,73 @@ const TablaPedidos = () => {
     toast.success("Producto eliminado");
   };
 
-  const handlerChange = (id, cantidad) => {
-    setPedidos((prevState) => {
-      prevState.map((item) =>
-        item._id === id ? { ...item, cantidadapedir: cantidad } : item
-      );
+ 
+
+  const deleteOrden = (id) => {
+    const res = orden.filter((e) => {
+      return e._id !== id;
     });
+    setOrden(res);
   };
 
-  const generarOrden = () => {
-    console.log(pedidos);
+
+  const handlerChange = (id, cantidad) => {
+    setPedidos((prevProductos) =>
+      prevProductos.map((producto) =>
+        producto._id === id ? { ...producto, cantidad } : producto
+      )
+    );
   };
+
+  const agregarAlCarrito = async() => {
+    // Filtra solo los productos con cantidades mayores a 0
+    const productosSeleccionados = pedidos.filter(
+      (producto) => producto.cantidad > 0
+    );
+
+    setOrden(productosSeleccionados);
+    // Enviar los productos al email de cada proveedor
+    // Reinicia las cantidades a 0 después de agregar al carrito
+    setPedidos((prevProductos) =>
+      prevProductos.map((producto) => ({ ...producto, cantidad: 0 }))
+    );
+  };
+
+  const ordenarPorNombre = () => {
+    let res = pedidos.sort((a, b) => {
+      return a.proveedor.localeCompare(b.proveedor, undefined, {
+        sensitivity: "base",
+      });
+    });
+
+    setPedidos(res);
+    router.refresh();
+  };
+
+  const enviarProductosAcomppra = async () => {
+    const res = await fetch(`${UrlWeb}/email`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+      },
+      body:JSON.stringify(orden)
+    });
+
+    if(!res.ok){
+      toast.error("Error al enviar el pedido")
+    }else{
+      toast.success("Pedido enviado satisfactoriamente")
+    setOrden([])
+
+    }
+
+  };
+
   return (
-    <div className="w-full h-full flex justify-center items-center">
-      {" "}
-      <button
-        onClick={generarOrden}
-        className="bg-green-900 py-2 px-4 rounded-md"
-      >
-        Add
-      </button>
+    <div className="w-full h-full flex justify-center items-center flex-col gap-8">
+      <div>
+        
+      </div>
       <table className="w-2/4 m-auto text-sm text-left text-gray-500 dark:text-gray-400 ">
         <thead className="text-xs text-slate-900 uppercase bg-sky-500 dark:bg-gray-900 dark:text-gray-400">
           <tr>
@@ -58,7 +108,11 @@ const TablaPedidos = () => {
             <th scope="col" className="px-6 py-3">
               Stock Minimo
             </th>
-            <th scope="col" className="px-6 py-3">
+            <th
+              scope="col"
+              className="px-6 py-3 cursor-pointer"
+              onClick={ordenarPorNombre}
+            >
               Proveedor
             </th>
 
@@ -81,11 +135,14 @@ const TablaPedidos = () => {
                 >
                   <td className="px-2 py-2 text-gray-900">
                     <input
-                      type="number"
-                      value={pedidos?.cantidadapedir}
+                      type="text"
+                      value={pedidos.cantidades}
                       className="w-[60px]"
                       onChange={(e) =>
-                        handlerChange(product._id, parseInt(e.target.value))
+                        handlerChange(
+                          product._id,
+                          parseFloat(e.target.value, 10)
+                        )
                       }
                     />
                   </td>
@@ -118,8 +175,53 @@ const TablaPedidos = () => {
               );
             })
           )}
+         <div className="flex gap-2">
+         <button
+            onClick={() => agregarAlCarrito()}
+            className=" rounded-md mt-2 p-2 bg-red-500 font-medium text-slate-50 dark:text-blue-500 hover:bg-sky-700"
+          >
+            add
+          </button>
+          <button
+            onClick={() => enviarProductosAcomppra()}
+            className=" rounded-md mt-2 p-2 bg-green-500 font-medium text-slate-50 dark:text-blue-500 hover:bg-sky-700"
+          >
+            Pedir
+          </button>
+         </div>
         </tbody>
       </table>
+      <table className=" w-3/4 border border-slate-500">
+        <thead>
+          <tr>
+            <th>Producto</th>
+            <th>Cantidad</th>
+          </tr>
+        </thead>
+        <tbody>
+          {orden?.map((e) => {
+            return (
+              <tr className=" border border-b-slate-700 ">
+                <td className="w-3/4"> {e.nombre} </td>
+                <td className="w-3/4">
+                  {" "}
+                  {e.cantidad}/{e.unidad}{" "}
+                </td>
+
+                <td className="p-2 block">
+                  <button
+                    className=" rounded-full p-2 bg-red-500 font-medium text-slate-50 dark:text-blue-500 hover:bg-sky-700"
+                    onClick={() => deleteOrden(e._id)}
+                  >
+                    <DeleteIcon />
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      {orden.length === 0 && <h1>No hay productos </h1>}
     </div>
   );
 };
