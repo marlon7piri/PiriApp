03d24d4317266e10646bd210cf36d8c2248d4923
Marlon Rodriguez Rivera <marlon7piri@gmail.com>
2025-03-13 17:54:26 -0500

    uoda

--- app/dashboard/mermas/FiltrosMermas.jsx
+++ app/dashboard/mermas/FiltrosMermas.jsx
@@ -2,50 +2,47 @@
 
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import React, { useState } from "react";
-import styles from '@/app/dashboard/productos/styles.module.css'
+import styles from "@/app/dashboard/productos/styles.module.css";
 import BotonComponent from "@/app/components/BotonComponent";
-
 
 const FiltrosMermas = () => {
   const [fecha, setFecha] = useState("");
   const { replace } = useRouter();
-  const searchparams = useSearchParams()
-  const path = usePathname()
-
+  const searchparams = useSearchParams();
+  const path = usePathname();
 
   const Filtrar = () => {
-    const params = new URLSearchParams(searchparams)
+    const params = new URLSearchParams(searchparams);
     if (fecha) {
-      params.set('fecha', fecha)
+      params.set("fecha", fecha);
     } else {
-      params.delete('fecha')
+      params.delete("fecha");
     }
 
-    replace(`${path}?${params}`)
-
-  }
+    replace(`${path}?${params}`);
+  };
 
   const limpiar = () => {
-    const params = new URLSearchParams(searchparams)
+    const params = new URLSearchParams(searchparams);
 
-    params.delete('fecha')
+    params.delete("fecha");
 
-
-    replace(`${path}?${params}`)
-
-
-  }
+    replace(`${path}?${params}`);
+  };
   return (
-    <div className={`flex items-center gap-4 bg-slate-50 shadow-2xl p-4 rounded-md mt-8 mb-8 ${styles.containerSearch}`}>
-      <input type="date" onChange={(e) => setFecha(e.target.value)} value={fecha} className="cursor-pointer" />
-      <div className={styles.btncontainer}>
-        <BotonComponent onClick={Filtrar} text='Filtrar'/>
-        <BotonComponent onClick={limpiar} text='Limpiar'/>
-        
-        
+    <div
+      className={`flex items-center gap-4 bg-slate-50 shadow-2xl p-4 rounded-md mt-8 mb-8 ${styles.containerSearch}`}
+    >
+      <input
+        type="date"
+        onChange={(e) => setFecha(e.target.value)}
+        value={fecha}
+        className="cursor-pointer"
+      />
+      <div className="w-full flex  gap-2 lg:flex-row sm:flex-col md:flex-col  sm:justify-center md:justify-center ">
+        <BotonComponent onClick={Filtrar} text="Filtrar" />
+        <BotonComponent onClick={limpiar} text="Limpiar" />
       </div>
-
-
     </div>
   );
 };
--- app/dashboard/platos/NavPlatos.jsx
+++ app/dashboard/platos/NavPlatos.jsx
@@ -10,14 +10,10 @@ const NavPlatos = () => {
     <div
       className={`flex  items-center gap-4 bg-slate-50 shadow-2xl p-4 rounded-md mt-8 mb-8 ${styles.containerSearch}`}
     >
-      <div className={styles.btncontainer}>
-        <div className="h-12 flex justify-center ">
-          <FiltroBusqueda />
-        </div>
-        <div>
-          <Boton texto="Nuevo" href="/dashboard/platos/new" />
-        </div>
+      <div className="h-12 flex justify-center ">
+        <FiltroBusqueda />
       </div>
+      <Boton texto="Nuevo" href="/dashboard/platos/new" />
     </div>
   );
 };
